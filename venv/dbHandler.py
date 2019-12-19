eaf574cd355d94c36b2207eb5a22a5430036c65a
ishayykk <ishayykk@gmail.com>
2019-12-19 11:03:11 +0200

    updates to db functions

--- venv/dbHandler.py
+++ venv/dbHandler.py
@@ -1,22 +1,65 @@
 from pymysql import *
 class db:
-    def init(self, conn_host, conn_user, conn_pw, conn_db, conn_cursor):
+    def __init__(self, conn_host, conn_user, conn_pw, conn_db, conn_cursor):
         self.host = conn_host
         self.user = conn_user
         self.password = conn_pw
         self.db = conn_db
         self.cursor = conn_cursor
         self.connection = None
-
     def db_connection(self):
         try:
             self.connection = connect(host=self.host, user=self.user, password = self.password, db = self.db, cursorclass = self.cursor)
-    def execute_query(self, query, num):
-        if(connection):
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                values = cursor.fetchall()  # Saving a list of dictionaries where each dictionary is a record
-                print("%s" % values)  # Get all records (fetchall)
-                # for value in values:
-                #     print(value['first_name'])
-                # print("The amount of students:", cursor.rowcount)  # Count the amount of rows being affected
+        except MySQLError:
+            print("DB Error")
+    def execute_query(self, query, type_of_query, table, num):
+        try:
+            self.db_connection()
+            if self.connection is not None:
+                with self.connection.cursor() as cursor:
+                    cursor.execute(query)
+                    if type_of_query == "SELECT":
+                        values = cursor.fetchall()  # Saving a list of dictionaries where each dictionary is a record
+                        lst = [field[0] for field in cursor.description]
+                        print()
+                        for value in values:
+                            if table == "customers":
+                                print(value['cust_id'],'\t\t', value['first_name'],'\t\t', value['last_name'],'\t\t', value['phone_num'])
+                            elif table == "projects":
+                                print("project id:", value['proj_id'], "start date:", value['start_date'], "cust id:",
+                                      value['cust_id'])
+                    elif type_of_query == "INSERT": # type 3/4 = INSERT query, commit is required
+                        self.connection.commit()
+                    # print("The amount of students:", cursor.rowcount)  # Count the amount of rows being affected\
+        except IntegrityError:
+            print("A new record couldn't be added")
+            return False
+        finally:
+            self.close_connection()  # Closing the opened connection
+        return True
+
+    def search_cust(self, id, query, table):
+        try:
+            self.connect_database()
+            if self.connection is not None:  # If the database connection was established successfully
+                self.connection.cursor().execute(query)
+                result = cursor.fetchone()
+                if result ==  id:
+                    print(table + id +" was found")
+                    return True
+                print(table + id + " wasn't found!")
+                return False
+        except IntegrityError:
+            print("A new record couldn't be added")
+            return False
+        finally:
+            self.close_connection()  # Closing the opened connection
+        return True
+
+    def close_connection(self):
+        """
+        Method is responsible for closing a database connection
+        :return: No return value
+        """
+        if self.connection is not None:
+            self.connection.close()
--- venv/main.py
+++ venv/main.py
@@ -5,24 +5,63 @@ def get_random_num():
     print(random)
     return random
 
+def print_menu():
+    print('''
+        1: print all customers details
+        2: print all projects
+        3: add a new customer
+        4: add a new project
+        5: exit 
+        ''')
+def get_queries(index):
+    if index == 1: return "SELECT * FROM customers"
+    elif index == 2: return "SELECT * FROM projects"
+    elif index == 3: return "SELECT cust_id FROM customers"
+    elif index == 4: return "INSERT INTO customers(cust_id, first_name, last_name, phone_num) VALUES(num, '%s', '%s', '%s')" % (
+                   input("Enter first name"), input("Enter last name"), input("enter phone number")),
+    elif index == 5: return "INSERT INTO projects(proj_id, proj_name, start_date, cust_id) VALUES(num, '%s', '%s', '%s')" % (
+                   input("Enter project name"), input("Enter prject start date"), input("enter customer id"))
+    else:
+        print("index out of bound")
+
 def main():
-    choice = input('''
-    1: print all customers details
-    2: print all projects
-    3: add a new customer
-    4: add a new project
-    5: exit 
-    ''')
-    db1 = dbHandler('127.0.0.1', 'root', None, 'company', cursors.DictCursor)
-    
-    queries = ["SELECT * FROM customers", "SELECT * FROM projects", "SELECT cust_id FROM customers", "SELECT proj_id FROM projects"]
-    while(choice !='5'):
-        if(choice == '1'):
-            db1.execute_query(queries[0])
-        if(choice == '2'):
-            db1.execute_query(queries[1])
-        if(choice == '3'):
-            db1.execute_query(queries[0], get_random_num())
+    choice = 0
+    db1 = db('127.0.0.1', 'root', None, 'company', cursors.DictCursor)
+
+    while choice !='5':
+        print_menu()
+        choice = input()
+        if choice == '1':
+            db1.execute_query(get_queries(1), "SELECT", "customers", 0)
+        elif choice == '2':
+            db1.execute_query(get_queries(1), "SELECT", "projects", 0)
+        elif choice == '3':
+            num = get_random_num()
+            while(db1.search_cust(num, get_queries(2), "Customer #")):
+                num = get_random_num()
+            db1.execute_query(get_queries(4), "INSERT", r)
+        elif choice == '4':
+            num = get_random_num()
+            while (not db1.search_cust(num, get_queries(3), "Project #")):
+                num = get_random_num()
+            db1.execute_query(get_queries(5), "INSERT", r)
+        elif choice == '5':
+            print("Exiting...")
+        else:
+            print("invalid option, please try again!\n")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 
 
 
